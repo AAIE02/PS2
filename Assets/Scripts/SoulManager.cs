f4153abe2b2eb58d21748edc004b44e90c262e30
AAIE02 <zap592@amerike.edu.mx>
2022-10-18 08:23:25 -0500

    a

--- Assets/Scripts/SoulManager.cs
+++ Assets/Scripts/SoulManager.cs
@@ -2,21 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PointManager : MonoBehaviour
+public class SoulManager : MonoBehaviour
 {
-
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-
+    // Update is called once per frame
     void Update()
     {
         
     }
 }
-
-// 1.- Bug de lanzamiento
-// 2.- Point Manager
-// 3.- Barra de HP del player y que los enemigos puedan causarle daño
