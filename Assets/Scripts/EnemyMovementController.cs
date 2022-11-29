92ad65c564112736d283f724d9fe1d6502d42656
Alfonso Arturo Ibarra Estrada <90528037+AAIE02@users.noreply.github.com>
2022-11-29 17:29:46 -0600

    EnemyMovementInPlataform

---
+++ Assets/Scripts/EnemyMovementController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class EnemyMovementController : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    [SerializeField] private Transform controladorDePlataforma;
+    [SerializeField] private float distance;
+    [SerializeField] private bool movimientoDerecha;
+    private Rigidbody2D rigi;
+
+    private void Start()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D infoPlataform = Physics2D.Raycast(controladorDePlataforma.position, Vector2.down, distance);
+        rigi.velocity = new Vector2(velocity, rigi.velocity.y);
+
+        if (infoPlataform == false)
+        {
+            FlipControlEnemy();
+        }
+    }
+
+    void FlipControlEnemy()
+    {
+        movimientoDerecha = !movimientoDerecha;
+        transform.eulerAngles = new Vector3(0,transform.eulerAngles.y + 180, 0);
+        velocity *= -1;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(controladorDePlataforma.transform.position, controladorDePlataforma.transform.position + Vector3.down * distance);
+    }
+
+}
